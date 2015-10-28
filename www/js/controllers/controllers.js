d858c72a8369eccc362532fb11c662c25acfd774
super <0@00010101.com>
2015-10-28 19:25:08 +0800

    ...

---
+++ www/js/controllers/controllers.js
@@ -0,0 +1,16 @@
+angular.module('app.controllers', [])
+.controller('HomeController',function($scope) {
+	console.log("home");
+})
+.controller('ListController',function($scope) {
+	console.log("list");
+})
+.controller('OrderController',function($scope) {
+	console.log("order");
+})
+.controller('UserController',function($scope) {
+	console.log("user");
+})
+.controller('DetailController',function($scope) {
+	console.log("detail");
+});
